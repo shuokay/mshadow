fccfd95d4a1b4ab24cce7230516553efb5062ef9
yushu.gao <shuokay@gmail.com>
2019-08-29 19:12:07 +0800

    since we have clip extension now, there is no need for mshadow::op::clip

--- mshadow/op.h
+++ mshadow/op.h
@@ -1,10 +1,10 @@
 /**
  * @file op.h
- * @author yushu.gao@horizon.ai
- * @brief define additional mshadow common math ops
- * 
+ * @author Yushu Gao
+ * @brief a collection of ops for F function
+ *
  * @copyright Copyright (c) 2019
- * 
+ *
  */
 #ifndef MSHADOW_OP_H_
 #define MSHADOW_OP_H_
@@ -51,30 +51,6 @@ struct ne {
   template <typename DType>
   MSHADOW_XINLINE static DType Map(DType a, DType b) {
     return a != b ? DType(1) : DType(0);
-  }
-};
-
-struct clip {
-  template <typename DType>
-  MSHADOW_XINLINE static DType Map(DType x, DType min, DType max) {
-    if (x < min) {
-      return min;
-    } else if (x > max) {
-      return max;
-    } else {
-      return x;
-    }
-  }
-  // Need to make sure bound >= 0
-  template <typename DType>
-  MSHADOW_XINLINE static DType Map(DType x, DType bound) {
-    if (x < -bound) {
-      return -bound;
-    } else if (x > bound) {
-      return bound;
-    } else {
-      return x;
-    }
   }
 };
 
